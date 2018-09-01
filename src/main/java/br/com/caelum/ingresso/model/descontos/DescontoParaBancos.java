77c36ec6074ed5c77a88cbe9578ba6b5e3edf1d7
Mohamad Saad <mohamad.saad@digitalpages.com.br>
2018-09-01 16:07:59 -0300

    add changes

--- src/main/java/br/com/caelum/ingresso/model/descontos/DescontoParaBancos.java
+++ src/main/java/br/com/caelum/ingresso/model/descontos/DescontoParaBancos.java
@@ -10,4 +10,9 @@ public class DescontoParaBancos implements Desconto {
     private BigDecimal trintaPorCentoSobre(BigDecimal precoOriginal) {
         return precoOriginal.multiply(new BigDecimal("0.3"));
     }
+    
+    @Override
+	public String getDescricao() {
+		return "Normal";
+	}
 }
